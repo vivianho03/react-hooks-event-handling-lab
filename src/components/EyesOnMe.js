bdc9c5301f3b85ab461869c7cc2f6d6169a378ca
Vivian Ho <ho.vivian89@gmail.com>
2023-01-21 00:39:07 -0800

    finished

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+export default function EyesOnMe(event){
+    return(
+        <button
+            onFocus={(event)=>{
+                console.log('Good!')
+            }}
+            onBlur={(event)=>{
+                console.log('Hey! Eyes on me!')
+            }}
+        >Eyes on me</button>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange(event){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input
+                type="password"
+                name="password"
+                onChange={handleChange}
+            />        
+        </div>
     )
 }
 
